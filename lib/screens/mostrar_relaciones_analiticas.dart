8b2d0678b7a91dd7e5f0e2721ad88dd834dba7fc
Angel Corrales <corralessoteloangel@outlook.com>
2022-06-02 22:27:15 -0600

    Pdf sin total cuentas

--- lib/screens/mostrar_relaciones_analiticas.dart
+++ lib/screens/mostrar_relaciones_analiticas.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend_test/model/value_objects/relaciones_analiticas.dart';
 import 'package:flutter_frontend_test/model/tools/convertidor_data_table.dart';
+import 'package:flutter_frontend_test/model/widgets/simple_elevated_button.dart';
 import 'package:flutter_frontend_test/screens/elegir_empresas.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:http/http.dart' as http;
 import '../env.sample.dart';
 import 'package:flutter_frontend_test/model/widgets/progress_bar.dart';
@@ -50,8 +53,8 @@ class RelacionesAnaliticasState extends State<MRelacionesAnaliticas> {
   List<DataCell> _createCells(datos) {
     List<DataCell> celdas = [];
 
-    //String type = datos[6].toString();
-    String type = "n";
+    String type = datos[6].toString();
+    // String type = "n";
 
     for (int i = 0; i < 6; i++) {
       if (type == "n") {
@@ -80,91 +83,93 @@ class RelacionesAnaliticasState extends State<MRelacionesAnaliticas> {
     PdfDocument document = PdfDocument();
 //Create a PdfGrid class
     PdfGrid grid = PdfGrid(); // Creación de la tabla
-    PdfGrid activoGrid = PdfGrid();
-    PdfGrid pasivoGrid = PdfGrid();
-    PdfGrid capitalGrid = PdfGrid();
+
 //Add the columns to the grid
     grid.columns
-        .add(count: 2); //Poner el número de columnas (RelacionesAnaliticas: 6)
-    activoGrid.columns.add(count: 2);
-    pasivoGrid.columns.add(count: 2);
-    capitalGrid.columns.add(count: 2);
+        .add(count: 6); //Poner el número de columnas (RelacionesAnaliticas: 6)
+
+    grid.columns[0].width = 66;
+    grid.columns[1].width = 130;
+    grid.columns[2].width = 84;
+    grid.columns[3].width = 76;
+    grid.columns[4].width = 76;
+    grid.columns[5].width = 84;
 //Add header to the grid
-    //grid.headers.add(1);
-    activoGrid.headers.add(1); //Agregar un header
-    pasivoGrid.headers.add(1);
-    capitalGrid.headers.add(1);
+    // grid.headers.add(2);
+    // grid.headers.add(1);
+
 //Add values to header
-    activoGrid.headers[0].cells[0].value = 'ACTIVO';
-    pasivoGrid.headers[0].cells[0].value = 'PASIVO';
-    capitalGrid.headers[0].cells[0].value = 'CAPITAL';
+    PdfGridRow header = grid.headers.add(1)[0];
+    header.cells[0].value = 'Cuenta';
+    header.cells[1].value = 'Nombre';
+    header.cells[2].value = 'Saldos iniciales\nDeudor Acreedor';
+    header.cells[3].value = 'Cargos';
+    header.cells[4].value = 'Abonos';
+    header.cells[5].value = 'Saldos Actuales\nDeudor Acreedor';
 
-    activoGrid.rows.add();
-    activoGrid.rows[0].cells[0].value = "Circulante";
+//Format cells
+    header.cells[0].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.left,
+        lineAlignment: PdfVerticalAlignment.top);
+
+    header.cells[1].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.left,
+        lineAlignment: PdfVerticalAlignment.top);
+
+    header.cells[1].style.textPen = PdfPens.mediumVioletRed;
+    header.cells[2].style.backgroundBrush = PdfBrushes.yellow;
+    header.cells[2].style.textBrush = PdfBrushes.darkOrange;
+
+    PdfGridRow row1 = grid.rows.add();
+    PdfGridRow row2 = grid.rows.add();
+
+    row1.cells[0].style = PdfGridCellStyle(
+      backgroundBrush: PdfBrushes.lightYellow,
+      cellPadding: PdfPaddings(left: 2, right: 3, top: 4, bottom: 5),
+      font: PdfStandardFont(PdfFontFamily.timesRoman, 17),
+      textBrush: PdfBrushes.white,
+      textPen: PdfPens.orange,
+    );
+
+    row2.cells[2].style.borders = PdfBorders(
+        left: PdfPen(PdfColor(240, 0, 0), width: 2),
+        top: PdfPen(PdfColor(0, 240, 0), width: 3),
+        bottom: PdfPen(PdfColor(0, 0, 240), width: 4),
+        right: PdfPen(PdfColor(240, 100, 240), width: 5));
     //data.ingreso.length
-    for (int i = 0; i < data.activo.circulante.length; i++) {
-      PdfGridRow curRow = activoGrid.rows.add();
+    for (int i = 0; i < data.movimientos.length; i++) {
+      PdfGridRow curRow = grid.rows.add();
       //data.ingreso[i][0] data.ingreso[i][1] data.ingreso[i][2]
-      curRow.cells[0].value = data.activo.circulante[i][0];
-      curRow.cells[1].value = data.activo.circulante[i][1];
-    }
+      curRow.cells[0].value = data.movimientos[i][0].toString();
+      curRow.cells[1].value = data.movimientos[i][1].toString();
+      curRow.cells[2].value = data.movimientos[i][2].toString();
+      curRow.cells[3].value = data.movimientos[i][3].toString();
+      curRow.cells[4].value = data.movimientos[i][4].toString();
+      curRow.cells[5].value = data.movimientos[i][5].toString();
 
+      //Si la linea va en negritas
+      if (data.movimientos[i][6] == 'n') {
+        curRow.style = PdfGridRowStyle(
+            font: PdfStandardFont(PdfFontFamily.timesRoman, 10,
+                style: PdfFontStyle.bold));
+      }
+
+      //Alineacion de lineas de saldos
+      curRow.cells[2].style.stringFormat =
+          PdfStringFormat(alignment: PdfTextAlignment.right);
+      curRow.cells[5].style.stringFormat =
+          PdfStringFormat(alignment: PdfTextAlignment.right);
+      //Set de padding de lineas de saldos
+      curRow.cells[2].style.cellPadding = PdfPaddings(right: 10);
+      curRow.cells[5].style.cellPadding = PdfPaddings(right: 10);
+      // Si es acreedora o deudora
+      if (data.movimientos[i][7] == 'a') {
+        curRow.cells[2].style.cellPadding = PdfPaddings(right: 20);
+        curRow.cells[5].style.cellPadding = PdfPaddings(right: 20);
+      }
+    }
     //LO DE  ANGEL SE QUEDA HASTA AQUI
-
-    activoGrid.rows.add();
-    activoGrid.rows[0].cells[0].value = "Fijo";
-
-    for (int i = 0; i < data.activo.fijo.length; i++) {
-      PdfGridRow curRow = activoGrid.rows.add();
-      curRow.cells[0].value = data.activo.fijo[i][0];
-      curRow.cells[1].value = data.activo.fijo[i][1];
-    }
-
-    activoGrid.rows.add();
-    activoGrid.rows[0].cells[0].value = "Diferido";
-
-    for (int i = 0; i < data.activo.diferido.length; i++) {
-      PdfGridRow curRow = activoGrid.rows.add();
-      curRow.cells[0].value = data.activo.diferido[i][0];
-      curRow.cells[1].value = data.activo.diferido[i][1];
-    }
-
-    pasivoGrid.rows.add();
-    pasivoGrid.rows[0].cells[0].value = "Circulante";
-
-    for (int i = 0; i < data.pasivo.circulante.length; i++) {
-      PdfGridRow curRow = pasivoGrid.rows.add();
-      curRow.cells[0].value = data.pasivo.circulante[i][0];
-      curRow.cells[1].value = data.pasivo.circulante[i][1];
-    }
-
-    pasivoGrid.rows.add();
-    pasivoGrid.rows[0].cells[0].value = "Fijo";
-
-    for (int i = 0; i < data.pasivo.fijo.length; i++) {
-      PdfGridRow curRow = pasivoGrid.rows.add();
-      curRow.cells[0].value = data.pasivo.fijo[i][0];
-      curRow.cells[1].value = data.pasivo.fijo[i][1];
-    }
-
-    pasivoGrid.rows.add();
-    pasivoGrid.rows[0].cells[0].value = "Diferido";
-
-    for (int i = 0; i < data.pasivo.diferido.length; i++) {
-      PdfGridRow curRow = pasivoGrid.rows.add();
-      curRow.cells[0].value = data.pasivo.diferido[i][0];
-      curRow.cells[1].value = data.pasivo.diferido[i][1];
-    }
-
-    capitalGrid.rows.add();
-    capitalGrid.rows[0].cells[0].value = "Capital";
-
-    for (int i = 0; i < data.capital.capital.length; i++) {
-      PdfGridRow curRow = capitalGrid.rows.add();
-      curRow.cells[0].value = data.capital.capital[i][0];
-      curRow.cells[1].value = data.capital.capital[i][1];
-    }
-
+    /*
 //Add rows to grid
     PdfGridRow row = grid.rows.add();
     row.cells[0].value = activoGrid;
@@ -172,16 +177,16 @@ class RelacionesAnaliticasState extends State<MRelacionesAnaliticas> {
     row = grid.rows.add();
     row.cells[0].value = '';
     row.cells[1].value = capitalGrid;
-
+ */
 //Set the grid style
 //AQUI VUELVES A GUIARTE
 
     grid.style = PdfGridStyle(
-        cellPadding: PdfPaddings(left: 2, right: 3, top: 4, bottom: 5),
+        cellPadding: PdfPaddings(left: 2, right: 3, top: 4, bottom: 0),
         backgroundBrush: PdfBrushes.white,
         textBrush: PdfBrushes.black,
-        borderOverlapStyle: PdfBorderOverlapStyle.inside,
-        font: PdfStandardFont(PdfFontFamily.timesRoman, 25));
+        borderOverlapStyle: PdfBorderOverlapStyle.overlap,
+        font: PdfStandardFont(PdfFontFamily.timesRoman, 10));
 //Draw the grid
     grid.draw(page: document.pages.add(), bounds: Rect.zero);
 //Save the document.
@@ -221,91 +226,127 @@ class RelacionesAnaliticasState extends State<MRelacionesAnaliticas> {
                 if (snapshot.hasData) {
                   developer.log('Uno', name: 'TieneData');
 
-                  return ListView(children: [
-                    SizedBox(height: screenHeight * .05),
-                    Center(
-                        child: Text(
-                      "Relaciones Analiticas",
-                      style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey.shade800,
-                          decoration: TextDecoration.none),
-                    )),
-                    SizedBox(height: screenHeight * .05),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  return ListView(
                       children: [
-                        Column(children: const [
-                          Text('FinRep',
-                              style:
-                                  TextStyle(color: Colors.blue, fontSize: 16))
-                        ]),
-                        Column(children: const [Text('Empresa 1 S.C')]),
-                        Column(children: const [Text('Fecha: 29/Abr/2022')]),
-                        Column(children: const [Text('Hola')])
-                      ],
-                    ),
-                    SizedBox(height: screenHeight * .12),
-                    Expanded(
-                      child: DataTable(
-                          columns: const <DataColumn>[
-                            DataColumn(
-                              label: Text(
-                                'Cuenta',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
+                            SizedBox(height: screenHeight * .05),
+                            Center(
+                                child: Text(
+                              "Relaciones Analiticas",
+                              style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey.shade800,
+                                  decoration: TextDecoration.none),
+                            )),
+                            SizedBox(height: screenHeight * .05),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(children: const [
+                                  Text('FinRep',
+                                      style: TextStyle(
+                                          color: Colors.blue, fontSize: 16))
+                                ]),
+                                Column(children: const [Text('Empresa 1 S.C')]),
+                                Column(children: const [
+                                  Text('Fecha: 29/Abr/2022')
+                                ]),
+                                Column(children: const [Text('Hola')])
+                              ],
                             ),
-                            DataColumn(
-                              label: Text(
-                                'Nombre',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            DataColumn(
-                              label: Text(
-                                'Saldos Iniciales \n Deudor  Acreedor',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            DataColumn(
-                              label: Text(
-                                '\n Cargos',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            DataColumn(
-                              label: Text(
-                                '\n Abonos',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            DataColumn(
-                              label: Text(
-                                'Saldos Actuales \n Deudor  Acreedor',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontWeight: FontWeight.bold),
-                              ),
+                            SizedBox(height: screenHeight * .12),
+                            Expanded(
+                              child: DataTable(
+                                  columns: const <DataColumn>[
+                                    DataColumn(
+                                      label: Text(
+                                        'Cuenta',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        'Nombre',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        'Saldos Iniciales \n Deudor  Acreedor',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        '\n Cargos',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        '\n Abonos',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    DataColumn(
+                                      label: Text(
+                                        'Saldos Actuales \n Deudor  Acreedor',
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    )
+                                  ],
+                                  rows: _createRows(datos.movimientos,
+                                      datos.totalCuentas, datos.sumasIguales)
+                                  //rows: createRows(snapshot.data?.ingresos),
+                                  ),
                             )
-                          ],
-                          rows: _createRows(datos.movimientos,
-                              datos.totalCuentas, datos.sumasIguales)
-                          //rows: createRows(snapshot.data?.ingresos),
-                          ),
-                    )
-                  ]);
+                          ] +
+                          [
+                            SimpleElevatedButton(
+                              child: const Text("Crear pdf falso"),
+                              color: Colors.blue,
+                              onPressed: () => gridsillo(snapshot.data),
+                              //getPDF(screenHeight, snapshot),
+                            ),
+                            const SizedBox(height: 25),
+                            SimpleElevatedButton(
+                              child: const Text("Volver"),
+                              color: Colors.red,
+                              onPressed: () => Get.back(),
+                              //getPDF(screenHeight, snapshot),
+                            ),
+                            const SizedBox(height: 25),
+                          ]);
                 } else {
-                  developer.log('${snapshot.error}', name: 'NoTieneData55');
-                  return const ProgressBar();
+                  // developer.log('${snapshot.error}', name: 'NoTieneData55');
+                  return ListView(children: [
+                    SimpleElevatedButton(
+                      child: const Text("Crear pdf falso"),
+                      color: Colors.blue,
+                      onPressed: () => gridsillo(snapshot.data),
+                      //getPDF(screenHeight, snapshot),
+                    ),
+                    const SizedBox(height: 25),
+                    SimpleElevatedButton(
+                      child: const Text("Volver"),
+                      color: Colors.red,
+                      onPressed: () => Get.back(),
+                      //getPDF(screenHeight, snapshot),
+                    ),
+                    const SizedBox(height: 25),
+                  ]);
+                  // return const ProgressBar();
                 }
               })),
     );
