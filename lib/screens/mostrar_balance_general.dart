17f001e5761aacff6c047f2671126fe14c9a45a5
Angel Corrales <corralessoteloangel@outlook.com>
2022-06-03 03:03:59 -0600

    pdf balance general sin bordes

--- lib/screens/mostrar_balance_general.dart
+++ lib/screens/mostrar_balance_general.dart
@@ -42,7 +42,7 @@ class BalanceGeneralState extends State<MBalanceGeneral> {
         name: 'idEmpresaDentrodeBalanceGeneral');
 
     final response = await http.get(Uri.parse(
-        "${Env.URL_PREFIX}/contabilidad/reportes/empresas/$idEmpresa/balance-general"));
+        "${Env.URL_PREFIX}/contabilidad/reportes/empresas/$idEmpresa/6/balance-general"));
     // await http.get(Uri.parse("${Env.URL_PREFIX}/balanceGeneral"));
 
     developer.log(jsonDecode(response.body).toString(), name: 'response18');
@@ -116,96 +116,185 @@ class BalanceGeneralState extends State<MBalanceGeneral> {
     PdfGrid activoGrid = PdfGrid();
     PdfGrid pasivoGrid = PdfGrid();
     PdfGrid capitalGrid = PdfGrid();
+    PdfGrid totalActivoGrid = PdfGrid();
 //Add the columns to the grid
     grid.columns
         .add(count: 2); //Poner el número de columnas (Estado de resultados: 5)
     activoGrid.columns.add(count: 2);
     pasivoGrid.columns.add(count: 2);
     capitalGrid.columns.add(count: 2);
+    totalActivoGrid.columns.add(count: 2);
 //Add header to the grid
     //grid.headers.add(1);
     activoGrid.headers.add(1); //Agregar un header
     pasivoGrid.headers.add(1);
     capitalGrid.headers.add(1);
+    totalActivoGrid.headers.add(1);
 //Add values to header
+    PdfBorders border = PdfBorders(
+        left: PdfPen(PdfColor(255, 255, 255), width: 0),
+        top: PdfPen(PdfColor(255, 255, 255), width: 0),
+        bottom: PdfPen(PdfColor(255, 255, 255), width: 0),
+        right: PdfPen(PdfColor(255, 255, 255), width: 0));
+
+    //Create a cell style
+    PdfGridCellStyle cellStyle = PdfGridCellStyle(
+      borders: border,
+    );
+
     activoGrid.headers[0].cells[0].value = 'ACTIVO';
     pasivoGrid.headers[0].cells[0].value = 'PASIVO';
     capitalGrid.headers[0].cells[0].value = 'CAPITAL';
+    totalActivoGrid.headers[0].cells[0].value = ' ';
+
+    activoGrid.headers[0].cells[0].style = cellStyle;
+    pasivoGrid.headers[0].cells[0].style = cellStyle;
+    capitalGrid.headers[0].cells[0].style = cellStyle;
+    totalActivoGrid.headers[0].cells[0].style = cellStyle;
+
+    activoGrid.headers[0].cells[1].style = cellStyle;
+    pasivoGrid.headers[0].cells[1].style = cellStyle;
+    capitalGrid.headers[0].cells[1].style = cellStyle;
+    totalActivoGrid.headers[0].cells[1].style = cellStyle;
 
     activoGrid.rows.add();
     activoGrid.rows[0].cells[0].value = "Circulante";
+    activoGrid.rows[0].cells[0].style = cellStyle;
+    activoGrid.rows[0].cells[1].style = cellStyle;
     //data.ingreso.length
     for (int i = 0; i < data.activo.circulante.length; i++) {
       PdfGridRow curRow = activoGrid.rows.add();
       //data.ingreso[i][0] data.ingreso[i][1] data.ingreso[i][2]
       curRow.cells[0].value = data.activo.circulante[i][0];
       curRow.cells[1].value = data.activo.circulante[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
     //LO DE ISAAC QUEDA HASTA AQUI
 
     activoGrid.rows.add();
     activoGrid.rows[0].cells[0].value = "Fijo";
+    activoGrid.rows[0].cells[0].style = cellStyle;
+    activoGrid.rows[0].cells[1].style = cellStyle;
 
     for (int i = 0; i < data.activo.fijo.length; i++) {
       PdfGridRow curRow = activoGrid.rows.add();
       curRow.cells[0].value = data.activo.fijo[i][0];
       curRow.cells[1].value = data.activo.fijo[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
     activoGrid.rows.add();
     activoGrid.rows[0].cells[0].value = "Diferido";
+    activoGrid.rows[0].cells[0].style = cellStyle;
+    activoGrid.rows[0].cells[1].style = cellStyle;
 
-    for (int i = 0; i < data.activo.diferido.length; i++) {
+    for (int i = 0; i < data.activo.diferido.length - 1; i++) {
       PdfGridRow curRow = activoGrid.rows.add();
       curRow.cells[0].value = data.activo.diferido[i][0];
       curRow.cells[1].value = data.activo.diferido[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
     pasivoGrid.rows.add();
     pasivoGrid.rows[0].cells[0].value = "Circulante";
+    pasivoGrid.rows[0].cells[0].style = cellStyle;
+    pasivoGrid.rows[0].cells[1].style = cellStyle;
 
     for (int i = 0; i < data.pasivo.circulante.length; i++) {
       PdfGridRow curRow = pasivoGrid.rows.add();
       curRow.cells[0].value = data.pasivo.circulante[i][0];
       curRow.cells[1].value = data.pasivo.circulante[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
     pasivoGrid.rows.add();
     pasivoGrid.rows[0].cells[0].value = "Fijo";
+    pasivoGrid.rows[0].cells[0].style = cellStyle;
+    pasivoGrid.rows[0].cells[1].style = cellStyle;
 
     for (int i = 0; i < data.pasivo.fijo.length; i++) {
       PdfGridRow curRow = pasivoGrid.rows.add();
       curRow.cells[0].value = data.pasivo.fijo[i][0];
       curRow.cells[1].value = data.pasivo.fijo[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
     pasivoGrid.rows.add();
     pasivoGrid.rows[0].cells[0].value = "Diferido";
+    pasivoGrid.rows[0].cells[0].style = cellStyle;
+    pasivoGrid.rows[0].cells[1].style = cellStyle;
 
     for (int i = 0; i < data.pasivo.diferido.length; i++) {
       PdfGridRow curRow = pasivoGrid.rows.add();
       curRow.cells[0].value = data.pasivo.diferido[i][0];
       curRow.cells[1].value = data.pasivo.diferido[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
+
+    totalActivoGrid.rows.add();
+    totalActivoGrid.rows[0].cells[0].value = ' ';
+    totalActivoGrid.rows[0].cells[0].style = cellStyle;
+    totalActivoGrid.rows[0].cells[1].style = cellStyle;
 
     capitalGrid.rows.add();
     capitalGrid.rows[0].cells[0].value = "Capital";
+    capitalGrid.rows[0].cells[0].style = cellStyle;
+    capitalGrid.rows[0].cells[1].style = cellStyle;
+    developer.log(
+        data.activo.diferido[data.activo.diferido.length - 1][0].toString(),
+        name: "buenas");
 
     for (int i = 0; i < data.capital.capital.length; i++) {
+      PdfGridRow curTotalRow = totalActivoGrid.rows.add();
+      if (data.capital.capital.length - 1 == i) {
+        developer.log("entroooo", name: "buenas");
+        curTotalRow.cells[0].value =
+            data.activo.diferido[data.activo.diferido.length - 1][0];
+        curTotalRow.cells[1].value =
+            data.activo.diferido[data.activo.diferido.length - 1][1];
+      } else {
+        curTotalRow.cells[0].value = ' ';
+        curTotalRow.cells[1].value = ' ';
+      }
+
+      curTotalRow.cells[0].style = cellStyle;
+      curTotalRow.cells[1].style = cellStyle;
+
       PdfGridRow curRow = capitalGrid.rows.add();
       curRow.cells[0].value = data.capital.capital[i][0];
       curRow.cells[1].value = data.capital.capital[i][1];
+
+      curRow.cells[0].style = cellStyle;
+      curRow.cells[1].style = cellStyle;
     }
 
 //Add rows to grid
     PdfGridRow row = grid.rows.add();
     row.cells[0].value = activoGrid;
     row.cells[1].value = pasivoGrid;
+
+    row.cells[0].style = cellStyle;
+    row.cells[1].style = cellStyle;
+
     row = grid.rows.add();
-    row.cells[0].value = '';
+    row.cells[0].value = totalActivoGrid;
     row.cells[1].value = capitalGrid;
 
+    row.cells[0].style = cellStyle;
+    row.cells[1].style = cellStyle;
 //Set the grid style
 //AQUI VUELVES A GUIARTE
 
@@ -213,7 +302,7 @@ class BalanceGeneralState extends State<MBalanceGeneral> {
         cellPadding: PdfPaddings(left: 2, right: 3, top: 4, bottom: 5),
         backgroundBrush: PdfBrushes.white,
         textBrush: PdfBrushes.black,
-        borderOverlapStyle: PdfBorderOverlapStyle.inside,
+        borderOverlapStyle: PdfBorderOverlapStyle.overlap,
         font: PdfStandardFont(PdfFontFamily.timesRoman, 25));
 //Draw the grid
     grid.draw(page: document.pages.add(), bounds: Rect.zero);
